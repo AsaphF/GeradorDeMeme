11a6fe89ac95f4f31695c52c291ca837dbd995e3
Asaph Ferreira <98856214+AsaphF@users.noreply.github.com>
2022-12-23 14:01:37 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "./componentes/Header"
+import Meme from "./componentes/Meme"
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Meme />
+        </div>
+    )
+}
